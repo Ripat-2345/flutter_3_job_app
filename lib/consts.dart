eae1f80534ccb24bbde35b52ce7f71d5a87da117
Ripat-2345 <ripatalsafar@gmail.com>
2022-06-25 10:51:57 +0700

    Make splash screen and add assets for app

---
+++ lib/consts.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = const Color(0xff06283D);
+Color secondaryColor = const Color(0xff1363DF);
+Color thirdColor = const Color(0xff1363DF);
+Color fourthColor = const Color(0xff1363DF);
+Color blackColor = Colors.black;
+Color whiteColor = Colors.white;
---
+++ lib/splash_screen.dart
@@ -0,0 +1,60 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_3_job_app/screens/home/home_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'consts.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(
+      const Duration(seconds: 2),
+      () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const HomeScreen()),
+        );
+      },
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: secondaryColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/icons/logo.png",
+              width: 59,
+              height: 78,
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Text(
+              "JOB APP",
+              style: GoogleFonts.poppins(
+                fontSize: 32,
+                fontWeight: FontWeight.w600,
+                color: whiteColor,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
